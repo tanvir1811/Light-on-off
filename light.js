f76d6d7d0e896694cbd4cfb8ea7df7a190a2337c
khan Tanvir Ferdous <168400554+tanvir1811@users.noreply.github.com>
2025-02-06 19:12:56 +0600

    Add files via upload

---
+++ light.js
@@ -0,0 +1,21 @@
+ 
+ 
+ function get() {
+
+     var imagej = document.getElementById("imgj");
+    imagej.src= "./img/lightson.png";
+
+
+ }
+ 
+ function gets() {
+
+   var imagej = document.getElementById("imgj");
+  imagej.src= "./img/lightsoff.png";
+
+
+}
+
+
+
+ 
